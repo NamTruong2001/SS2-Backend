ab3b018184f47d58c3fe706cadc4caf2a79ba61a
truongnn-fv <truong_nn@flinters.vn>
2023-02-24 13:57:13 +0700

    set up code base for SS2 project

---
+++ src/main/java/com/ss2fit/ss2backend/Ss2BackendApplication.java
@@ -0,0 +1,13 @@
+package com.ss2fit.ss2backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ss2BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ss2BackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ss2fit/ss2backend/Ss2BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ss2fit.ss2backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ss2BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
