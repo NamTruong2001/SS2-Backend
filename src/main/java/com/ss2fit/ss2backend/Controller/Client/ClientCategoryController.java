0b1ff7e1196ff66f9a8b2cbead68cf4c2f7681c9
truongnn-fv <truong_nn@flinters.vn>
2023-03-04 09:29:19 +0700

    edit controller objects

--- src/main/java/com/ss2fit/ss2backend/Controller/Client/ClientCategoryController.java
+++
@@ -1,4 +0,0 @@
-package com.ss2fit.ss2backend.Controller.Client;
-
-public class ClientCategoryController {
-}
--- src/main/java/com/ss2fit/ss2backend/Controller/ProductController.java
+++ src/main/java/com/ss2fit/ss2backend/Controller/ProductController.java
@@ -1,28 +1,39 @@
-package com.ss2fit.ss2backend.Controller.Client;
+package com.ss2fit.ss2backend.Controller;
 
+import com.ss2fit.ss2backend.DTO.CreateDiscountDTO;
+import com.ss2fit.ss2backend.DTO.CreateProductDTO;
 import com.ss2fit.ss2backend.DTO.ProductDTO;
 import com.ss2fit.ss2backend.Model.Product;
-import com.ss2fit.ss2backend.Repository.CategoryRepository;
 import com.ss2fit.ss2backend.Repository.ProductRepository;
+import com.ss2fit.ss2backend.Service.DiscountService;
+import com.ss2fit.ss2backend.Service.FilesStorageService;
 import com.ss2fit.ss2backend.Service.ProductService;
 import com.ss2fit.ss2backend.utils.Exceptions.CategoryNotFoundException;
 import com.ss2fit.ss2backend.utils.Exceptions.ProductNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.nio.file.Paths;
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/product/")
-public class ClientProductController {
+public class ProductController {
     @Autowired
     private ProductService productService;
     @Autowired
     private ProductRepository productRepository;
+    @Autowired
+    private FilesStorageService filesStorageService;
+    @Autowired
+    private DiscountService discountService;
     @GetMapping("/products")
     public List<ProductDTO> getAllProducts() {
         return productService.getAllProducts();
@@ -52,6 +63,47 @@ public class ClientProductController {
         } catch (CategoryNotFoundException e) {
             return new ResponseEntity("Category Not Found", HttpStatus.NOT_FOUND);
         } catch (Exception e) {
+            return new ResponseEntity(e.getMessage(), HttpStatus.FORBIDDEN);
+        }
+    }
+
+    @PostMapping("/create-product")
+    public String createProduct(@RequestHeader String host,
+                                @ModelAttribute CreateProductDTO createProductDTO,
+                                @RequestParam("images") MultipartFile[] multipartFile) {
+
+        List<String> imagesList = Arrays.stream(multipartFile).map(
+                file -> {
+                    try {
+                        return host + filesStorageService.save(file);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+        ).collect(Collectors.toList());
+        productService.createProduct(createProductDTO, imagesList);
+        return "OK";
+    }
+
+    @PostMapping("/apply-discount")
+    public ResponseEntity createAndApplyDiscount(@RequestBody CreateDiscountDTO createDiscountDTO) {
+        try {
+            discountService.createAndApplyDiscount(createDiscountDTO.getDiscountDTO(), createDiscountDTO.getProductIds());
+
+            return new ResponseEntity<>("OK", HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.FORBIDDEN);
+        }
+    }
+
+    @PostMapping("/delete-product")
+    public ResponseEntity deleteProduct(@RequestParam("productId") String productId) {
+        try {
+            productService.deleteProduct(productId);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (EmptyResultDataAccessException err) {
+            return new ResponseEntity("Product Not Found", HttpStatus.NOT_FOUND);
+        }  catch (Exception e) {
             return new ResponseEntity(e.getMessage(), HttpStatus.FORBIDDEN);
         }
     }
