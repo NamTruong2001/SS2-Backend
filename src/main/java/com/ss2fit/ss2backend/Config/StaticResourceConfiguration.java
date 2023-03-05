359c9099f0511fe39ef40d9312bd7ebc85d5e160
truongnn-fv <truong_nn@flinters.vn>
2023-03-05 09:23:25 +0700

    implement read image

--- src/main/java/com/ss2fit/ss2backend/Config/StaticResourceConfiguration.java
+++ src/main/java/com/ss2fit/ss2backend/Config/StaticResourceConfiguration.java
@@ -8,8 +8,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class StaticResourceConfiguration implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/images/**").addResourceLocations("classpath:/static/images/");
-
         registry.addResourceHandler("swagger-ui.html")
                 .addResourceLocations("classpath:/META-INF/resources/");
 
